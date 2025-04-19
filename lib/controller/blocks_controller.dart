6916e0f0dd043f127dd5a97e4c2f95e287cc6fc7
Husam Dahliz <husamdahliz@gmail.com>
2025-04-19 09:22:35 +0300

    refactor: Remove unused permission_handler dependency and clean up saveNetworkImage method

--- lib/controller/blocks_controller.dart
+++ lib/controller/blocks_controller.dart
@@ -1,10 +1,6 @@
-import 'dart:io';
-
 import 'package:dio/dio.dart';
-import 'package:flutter/material.dart';
 import 'package:gallery_saver_plus/gallery_saver.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:permission_handler/permission_handler.dart';
 
 import '../../views/widgets/loading_dialog.dart';
 import '../../views/widgets/snack.dart';
@@ -71,71 +67,16 @@ class BlocksController extends GetxController {
     );
   }
 
-  // Future<void> saveNetworkImage(String imageUrl) async {
-  //   try {
-  //     if (!await Permission.storage.request().isGranted) {
-  //       Snack().show(type: false, message: "مطلوب صلاحية الوصول للتخزين");
-  //       return;
-  //     }
-  //     LoadingDialog().dialog();
-  //     final response = await Dio().get(
-  //       imageUrl,
-  //       options: Options(responseType: ResponseType.bytes),
-  //     );
-  //     final result = await ImageGallerySaver.saveImage(
-  //       Uint8List.fromList(response.data),
-  //       quality: 100,
-  //       name: "image_${DateTime.now().millisecondsSinceEpoch}",
-  //     );
-
-  //     Get.back();
-
-  //     if (result['isSuccess'] == true || result['filePath'] != null) {
-  //       Snack().show(type: true, message: "تم الحفظ في مكتبة الصور");
-  //     } else {
-  //       Snack().show(type: false, message: "فشل في حفظ الصورة");
-  //     }
-  //   } catch (e) {
-  //     Get.back();
-  //     Snack().show(type: false, message: "حدث خطأ أثناء الحفظ");
-  //   }
-  // }
-
   Future<void> saveNetworkImage(String imageUrl) async {
     try {
-      final status = await Permission.manageExternalStorage.request();
-
-      if (status.isPermanentlyDenied) {
-        await showDialog(
-          context: Get.context!,
-          builder: (_) => AlertDialog(
-            title: Text("الصلاحيات مطلوبة"),
-            content: Text("يرجى تفعيل صلاحية التخزين من إعدادات التطبيق."),
-            actions: [
-              TextButton(
-                child: Text("فتح الإعدادات"),
-                onPressed: () {
-                  openAppSettings();
-                  Get.back();
-                },
-              ),
-              TextButton(
-                child: Text("إلغاء"),
-                onPressed: () => Get.back(),
-              ),
-            ],
-          ),
-        );
-        return;
-      } else if (status.isDenied) {
-        Snack().show(type: false, message: "مطلوب صلاحية الوصول للتخزين");
-        return;
-      }
-
       LoadingDialog().dialog();
 
-      final response = await Dio().get(
+      final tempDir = await getTemporaryDirectory();
+      final path =
+          '${tempDir.path}/image_${DateTime.now().millisecondsSinceEpoch}.jpg';
+      await Dio().download(
         imageUrl,
+        path,
         options: Options(responseType: ResponseType.bytes),
         onReceiveProgress: (received, total) {
           if (total != -1) {
@@ -145,24 +86,12 @@ class BlocksController extends GetxController {
         },
       );
 
-      final tempDir = await getTemporaryDirectory();
-      final filePath =
-          '${tempDir.path}/image_${DateTime.now().millisecondsSinceEpoch}.jpg';
-      final file = File(filePath);
-      await file.writeAsBytes(response.data);
-
-      bool success = await GallerySaver.saveImage(file.path) ?? false;
-
+      await GallerySaver.saveImage(path, albumName: 'Baiti') ?? false;
       Get.back();
-
-      if (success) {
-        Snack().show(type: true, message: "تم حفظ الصورة في المعرض");
-      } else {
-        Snack().show(type: false, message: "حذث خطأ أثناء حفظ الصورة");
-      }
+      Snack().show(type: true, message: "تم حفظ الصورة في المعرض");
     } catch (e) {
       Get.back();
-      Snack().show(type: false, message: "حدث خطأ أثناء الحفظ");
+      Snack().show(type: false, message: "حدث خطأ أثناء حفظ الصورة");
     }
   }
 }
