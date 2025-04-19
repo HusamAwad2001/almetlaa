51178e5623cfb8b032cdb775bcec6207d451c75f
Husam Dahliz <husamdahliz@gmail.com>
2025-04-19 14:07:17 +0300

    refactor: Simplify news display layout and enhance scroll behavior in NewsPage

--- lib/views/pages/news_page.dart
+++ lib/views/pages/news_page.dart
@@ -73,79 +73,32 @@ class _NewsPageState extends State<NewsPage> {
                             style: TextStyle(fontSize: 16),
                           ),
                         )
-                      : Column(
-                          children: [
-                            30.ph,
-                            TextField(
-                              onSubmitted: (v) {
-                                if (v.isEmpty) {
-                                  controller.getNews();
-                                } else {
-                                  controller.searchNews(v);
-                                }
-                              },
-                              decoration: InputDecoration(
-                                hintText: 'البحث',
-                                prefixIcon: Container(
-                                  width: 22.w,
-                                  height: 22.h,
-                                  alignment: Alignment.center,
-                                  child: Image.asset(
-                                    'assets/images/search.png',
-                                    width: 22.w,
-                                    height: 22.h,
-                                    color: Constants.primaryColor,
-                                  ),
-                                ),
-                                border: InputBorder.none,
-                                contentPadding: EdgeInsets.zero,
-                                enabledBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(9.r),
-                                  borderSide: const BorderSide(
-                                      width: 1, color: Color(0xFFF0F0F0)),
-                                ),
-                                focusedBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(9.r),
-                                  borderSide: const BorderSide(
-                                      width: 1, color: Color(0xFFF0F0F0)),
-                                ),
-                                filled: true,
-                                fillColor: const Color(0xFFF5F5F5),
+                      : SingleChildScrollView(
+                          controller: scrollController,
+                          child: Column(
+                            children: [
+                              AlignedGridView.count(
+                                physics: const NeverScrollableScrollPhysics(),
+                                padding: const EdgeInsets.all(25),
+                                mainAxisSpacing: 25.h,
+                                crossAxisSpacing: 25.w,
+                                shrinkWrap: true,
+                                itemCount: controller.news.length,
+                                crossAxisCount: 2,
+                                itemBuilder: (_, index) {
+                                  final item = controller.news[index];
+                                  return HomeNewsWidget(item: item);
+                                },
                               ),
-                            ),
-                            30.ph,
-                            Expanded(
-                              child: SingleChildScrollView(
-                                controller: scrollController,
-                                child: Column(
-                                  children: [
-                                    AlignedGridView.count(
-                                      physics:
-                                          const NeverScrollableScrollPhysics(),
-                                      padding:
-                                          const EdgeInsets.only(bottom: 20),
-                                      mainAxisSpacing: 25.h,
-                                      crossAxisSpacing: 25.w,
-                                      shrinkWrap: true,
-                                      itemCount: controller.news.length,
-                                      crossAxisCount: 2,
-                                      itemBuilder: (_, index) {
-                                        final item = controller.news[index];
-                                        return HomeNewsWidget(item: item);
-                                      },
-                                    ),
-                                    if (controller.hasMore &&
-                                        controller.news.isNotEmpty)
-                                      Padding(
-                                        padding: const EdgeInsets.all(20.0),
-                                        child: CircularProgressIndicator(),
-                                      ),
-                                  ],
+                              if (controller.hasMore &&
+                                  controller.news.isNotEmpty)
+                                Padding(
+                                  padding: const EdgeInsets.all(20.0),
+                                  child: CircularProgressIndicator(),
                                 ),
-                              ),
-                            ),
-                          ],
-                        ).paddingSymmetric(horizontal: 25.w);
+                            ],
+                          ),
+                        );
         },
       ),
     );
